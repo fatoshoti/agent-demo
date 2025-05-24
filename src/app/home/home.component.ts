56e7eb9468e73de52ecd2ad75e5ba26137ce0cae
fatoshoti <fatoshoti@fatoshoti-macbookpro2.roam.internal>
2025-05-24 13:16:29 +0200

    feat: Created HomeComponent with image gallery

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  imageUrls: string[] = [
+    'assets/image1.jpg',
+    'assets/image2.jpg',
+    'assets/image3.jpg',
+    'assets/image4.jpg',
+    'assets/image5.jpg',
+    'assets/image6.jpg'
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
