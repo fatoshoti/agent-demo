500e6474c444d2d3e02ed9f6ef4ba0d7b74423f2
fatoshoti <hoti.fatos@gmail.com>
2025-05-25 20:36:09 +0200

    reset

--- frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: '**', redirectTo: '' } // Redirect to home for any other invalid routes
-];
